d04bcb3d5e5b90355db76c34cb98312c91f875f8
Unknown <talyon56@hotmail.fr>
2018-06-26 16:21:59 +0200

    Commit SS
    Selection Par nom + Mdp

--- src/fr/eni/clinique/dao/GetValuesDataBase.java
+++ src/fr/eni/clinique/dao/GetValuesDataBase.java
@@ -5,6 +5,15 @@
  */
 package fr.eni.clinique.dao;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Administrateur
@@ -13,8 +22,21 @@ public class GetValuesDataBase {
     
     
     public boolean getIdentifiant(String user, String password){
-        
-        return false;
+        ConnexionDataBase co = new ConnexionDataBase();                
+        Connection connect = co.getCo();
+        PreparedStatement stm;
+        ResultSet rs;
+        try {
+            stm = connect.prepareStatement("select * from Personnels where nom=" + user + " and MotPasse=" + password);
+            rs = stm.executeQuery();
+            if(rs.next())
+                return true;
+            else
+                return false;            
+        } catch (SQLException ex) {
+            Logger.getLogger(GetValuesDataBase.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
     }
     
 }
