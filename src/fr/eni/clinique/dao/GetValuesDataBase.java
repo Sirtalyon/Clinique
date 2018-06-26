facdac8228091a89d1d1c05e8cca8655f82271c5
Unknown <talyon56@hotmail.fr>
2018-06-26 15:50:08 +0200

    Commit SS
    Classe Recherche BDD

---
+++ src/fr/eni/clinique/dao/GetValuesDataBase.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.eni.clinique.dao;
+
+/**
+ *
+ * @author Administrateur
+ */
+public class GetValuesDataBase {
+    
+    
+    public boolean getIdentifiant(String user, String password){
+        
+        return false;
+    }
+    
+}
