b2aacf379056215dafc4f35d64c70183a259a086
Unknown <talyon56@hotmail.fr>
2018-06-26 15:19:09 +0200

    Commit SS
    Connexion BDD + fichier settings

--- src/fr/eni/clinique/dao/ConnexionDataBase.java
+++ src/fr/eni/clinique/dao/ConnexionDataBase.java
@@ -1,30 +1,30 @@
 package fr.eni.clinique.dao;
 
+import fr.eni.clinique.properties.Configuration;
 import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public class ConnexionDataBase {
 
-    public static void main(String[] args) throws IllegalAccessException, InstantiationException {
+    public static void main(String[] args){
         try {
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            Class.forName(Configuration.getValue("driver"));
         } catch (ClassNotFoundException e) {
             System.out.println("Erreur pendant le chargement du pilote");
         }
         Connection co = null;
-        try{
-            co = DriverManager.getConnection("jdbc:sqlserver://148-UC48-08;databaseName=master; user=sa;password=Pa$$w0rd;");
-            if(co != null)
-            {
+        String url = Configuration.getValue("url");
+        String user = Configuration.getValue("user");
+        String password = Configuration.getValue("password");
+        try {
+            co = DriverManager.getConnection(url, user, password);
+            if (co != null) {
                 System.out.println("OK");
             }
-        }catch (SQLException e)
-        {
+        } catch (SQLException e) {
             System.out.println("Erreur pendant la connexion");
         }
-
-
 
     }
 }
---
+++ src/fr/eni/clinique/properties/Configuration.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.eni.clinique.properties;
+
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+/**
+ *
+ * @author Administrateur
+ */
+public class Configuration {
+    private static final String BUNDLE_NAME = "fr.eni.clinique.properties"; 
+	
+	//Objet qui va lire le contenu du fichier
+	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
+
+	private Configuration() {
+		
+	}
+	
+	public static String getValue(String key) {
+		try {
+			return RESOURCE_BUNDLE.getString(key);
+		} catch (MissingResourceException e) {
+			return '!' + key + '!';
+		}
+	}
+}
