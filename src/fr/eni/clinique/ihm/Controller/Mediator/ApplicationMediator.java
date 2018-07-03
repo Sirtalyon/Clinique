68ff134318e096b248b7ff1a962a38ad38a2d8eb
Unknown <dofus2900@gmail.com>
2018-07-03 14:55:26 +0200

    commitPL
    add

---
+++ src/fr/eni/clinique/ihm/Controller/Mediator/ApplicationMediator.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.eni.clinique.ihm.Controller.Mediator;
+
+/**
+ *
+ * @author plaurent2017
+ */
+public class ApplicationMediator implements IMediator {
+
+    @Override
+    public void send() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
